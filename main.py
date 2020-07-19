aebbd96506a7045b032e1c22abd1c69a1bc66d79
Gatien Oudoire <37880199+Gatien-Oudoire@users.noreply.github.com>
2020-07-19 20:37:46 +0200

    Ajout du menu A Propos

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 #Ressources
 import tkinter
 
-# Fenetre et parametres
+# Fenetres et parametres
 app = tkinter.Tk()
 app.geometry("1366x768")
 app.title("Lecteur Vidéo VLB2")
+
+#Fonction -> Fenetre A propos
+def apropos():
+    fenetreApropos = tkinter.Toplevel(app)
+    fenetreApropos.title("A Propos")
+    fenetreApropos.geometry("250x250")
+    auteur = tkinter.Label(fenetreApropos, text="Auteur: Gatien Oudoire")
+    auteur.pack()
 
 #Decalaration de barre de menu
 mainMenu = tkinter.Menu(app)
 
 #Menu FICHIER
-menuFichier = tkinter.Menu(mainMenu)
+menuFichier = tkinter.Menu(mainMenu, tearoff=0)
 menuFichier.add_command(label="Ouvrir un fichier")
 menuFichier.add_command(label="Quitter", command=app.quit)
 
 #Menu AIDE
-menuAide = tkinter.Menu(mainMenu)
-menuAide.add_command(label="A propos")
+menuAide = tkinter.Menu(mainMenu, tearoff=0)
+menuAide.add_command(label="A propos", command=apropos)
 
 #Ajout des differents menus
 mainMenu.add_cascade(label="Fichier",menu=menuFichier)
-mainMenu.add_cascade(label="Fichier",menu=menuAide)
+mainMenu.add_cascade(label="A propos",menu=menuAide)
 
 #Boucle de la fenetre
 app.config(menu=mainMenu)
 app.mainloop()
-
-
