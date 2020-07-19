6b0ef35a7811de8f5f23ae77e87818ca878b3fb5
Gatien Oudoire <37880199+Gatien-Oudoire@users.noreply.github.com>
2020-07-19 23:13:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 #Ressources
 import tkinter
+import webbrowser
 
 # Fenetres et parametres
 app = tkinter.Tk()
 app.geometry("1366x768")
 app.title("Lecteur Vidéo VLB2")
 
-#Fonction -> Fenetre A propos
+#Fonctions -> Fenetre A propos
 def apropos():
     fenetreApropos = tkinter.Toplevel(app)
     fenetreApropos.title("A Propos")
     fenetreApropos.geometry("250x250")
     auteur = tkinter.Label(fenetreApropos, text="Auteur: Gatien Oudoire")
     auteur.pack()
+def support():
+    webbrowser.open_new(r"https://gatien-oudoire.com")
 
 #Decalaration de barre de menu
 mainMenu = tkinter.Menu(app)
@@ -20,10 +23,12 @@ mainMenu = tkinter.Menu(app)
 #Menu FICHIER
 menuFichier = tkinter.Menu(mainMenu, tearoff=0)
 menuFichier.add_command(label="Ouvrir un fichier")
+menuFichier.add_separator()
 menuFichier.add_command(label="Quitter", command=app.quit)
 
 #Menu AIDE
 menuAide = tkinter.Menu(mainMenu, tearoff=0)
+menuAide.add_command(label="Support", command=support)
 menuAide.add_command(label="A propos", command=apropos)
 
 #Ajout des differents menus
