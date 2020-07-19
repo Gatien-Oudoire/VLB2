51ab6777e6cd3c6e1920057d240a50ae54fff7af
Gatien Oudoire <37880199+Gatien-Oudoire@users.noreply.github.com>
2020-07-19 20:21:34 +0200

    Création d un menu et ajout de commentaire
    Les boutons des menus déroulant ne marches mis à part "Quitter"

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from tkinter import *
+#Ressources
+import tkinter
 
-fenetrePrincipal = Tk()
+# Fenetre et parametres
+app = tkinter.Tk()
+app.geometry("1366x768")
+app.title("Lecteur Vidéo VLB2")
 
-listeFichier = Listbox(fenetrePrincipal)
+#Decalaration de barre de menu
+mainMenu = tkinter.Menu(app)
 
-listeFichier.insert(END, "Fichier")
-listeFichier.insert(END, "Ouvrir un fichier")
+#Menu FICHIER
+menuFichier = tkinter.Menu(mainMenu)
+menuFichier.add_command(label="Ouvrir un fichier")
+menuFichier.add_command(label="Quitter", command=app.quit)
 
-listeFichier.pack()
+#Menu AIDE
+menuAide = tkinter.Menu(mainMenu)
+menuAide.add_command(label="A propos")
 
-fenetrePrincipal.mainloop()
+#Ajout des differents menus
+mainMenu.add_cascade(label="Fichier",menu=menuFichier)
+mainMenu.add_cascade(label="Fichier",menu=menuAide)
+
+#Boucle de la fenetre
+app.config(menu=mainMenu)
+app.mainloop()
+
+
