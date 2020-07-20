618b08f39120c0471b7e124931f15b08741ee85d
Gatien Oudoire <37880199+Gatien-Oudoire@users.noreply.github.com>
2020-07-20 15:45:01 +0200

    Ajouts de menus

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from tkinter import filedialog
 import tkinter
 import webbrowser
 import cv2
+import numpy as np
+from pydub import AudioSegment
+from pydub.playback import play
 
 
 # Fenetres et parametres
 lecteurVideo = tkinter.Tk()
 lecteurVideo.geometry("1420x820")
+lecteurVideo.minsize(700,400)
 lecteurVideo.title("Lecteur Vidéo VLB2")
 
 #Fonction Fichier
@@ -16,8 +20,9 @@ def ouvrirFichier():
     #Liste des types de fichiers supportés
     typesDeFichier = [
         ('Fichiers Vidéos', '*.mp4 *.avi'),
-        ('Fichiers Audio', '*.mp3')
+        ('Fichiers Audio', '*.mp3 *.wav')
     ]
+    
     #Demande à l utilisateur de choisir le fichier
     fichierPath = filedialog.askopenfilename(filetypes=typesDeFichier)
     print(fichierPath)
@@ -29,7 +34,26 @@ def ouvrirFichier():
         separateur = fichierPath.split(".")
         formatVideo = separateur[-1]
         print(formatVideo)
+
+        #Lance le son
+        if formatVideo == "mp3":
+            fichierAudio = AudioSegment.from_mp3(fichierPath)
+        elif formatVideo == "wav":
+            fichierAudio = AudioSegment.from_wav(fichierPath)
+        play(fichierAudio)
+                    
+def camera():
+    fluxVideo = cv2.VideoCapture(0)
+    while True:
+
+        ret, frame = fluxVideo.read()
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
         
+        fluxVideo.read()
+        cv2.destroyAllWindows()
 
 #Fonctions -> Fenetre A propos
 def apropos():
@@ -47,8 +71,22 @@ mainMenu = tkinter.Menu(lecteurVideo)
 #Menu FICHIER
 menuFichier = tkinter.Menu(mainMenu, tearoff=0)
 menuFichier.add_command(label="Ouvrir un fichier", command=ouvrirFichier)
+menuFichier.add_command(label="Utiliser une caméra", command=camera)
+menuFichier.add_cascade(label="Fichiers récents")
 menuFichier.add_separator()
 menuFichier.add_command(label="Quitter", command=lecteurVideo.quit)
+
+#MENU LECTURE
+menuLecture = tkinter.Menu(mainMenu, tearoff=0)
+menuLecture.add_command(label="Lire")
+menuLecture.add_command(label="Pause")
+menuLecture.add_command(label="Plein Ecran")
+
+#MENU SON
+menuSon = tkinter.Menu(mainMenu, tearoff=0)
+menuSon.add_command(label="Augmenter volume")
+menuSon.add_command(label="Diminuer Volume")
+menuSon.add_command(label="Couper le son")
 
 #Menu AIDE
 menuAide = tkinter.Menu(mainMenu, tearoff=0)
@@ -57,15 +95,17 @@ menuAide.add_command(label="A propos", command=apropos)
 
 #Ajout des differents menus
 mainMenu.add_cascade(label="Fichier",menu=menuFichier)
+mainMenu.add_cascade(label="Lecture",menu=menuLecture)
+mainMenu.add_cascade(label="Son",menu=menuSon)
 mainMenu.add_cascade(label="A propos",menu=menuAide)
 
-cadre = tkinter.Frame(lecteurVideo, bg="black", width=1280, height=720)
+#Création du cadre ou sera lu la vidéo
+cadreVideo = tkinter.Frame(lecteurVideo, bg="black", width=1280, height=720)
 
-barreControle = tkinter.Frame(lecteurVideo, bg="grey")
+
+#Barre pour controler la vidéo
 
 #Boucle de la fenetre
-cadre.pack()
-barreControle.propagate(False)
-barreControle.pack()
+cadreVideo.pack()
 lecteurVideo.config(menu=mainMenu)
 lecteurVideo.mainloop()
